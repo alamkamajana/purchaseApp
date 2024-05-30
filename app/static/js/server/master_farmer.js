62996e955cef270431845f66b12294d0078d6081
Cahya Nurhadinata <nurhadinata8@gmail.com>
2024-05-30 13:47:36 +0800

    update master farmer js

--- app/static/js/server/master_farmer.js
+++ app/static/js/server/master_farmer.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
+
+    var selectedStation = $('#station_id').val();
+    var index=0;
+    var first_option=0;
+    // $('#farmer_group_form').val(null).trigger('change');
+    $('#parent_id option').each(function() {
+        var itemStation = $(this).data('category');
+        if (selectedStation == 'all' || selectedStation == itemStation) {
+            if(index==0){
+                first_option=$(this).val()
+            }
+            $(this).show();
+            index++
+        } else {
+            $(this).hide();
+        }
+    });
+    $('#parent_id').val(first_option).trigger('change');
     
     $('#station_id').on('change', function() {
         var selectedStation = $(this).val();
         $('#parent_id').val(null).trigger('change');
+        var index=0;
+        var first_option=0;
         $('#parent_id option').each(function() {
-        var itemStation = $(this).data('category');
-        if (selectedStation == 'all' || selectedStation == itemStation) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
+            var itemStation = $(this).data('category');
+            if (selectedStation == 'all' || selectedStation == itemStation) {
+                if(index==0){
+                    first_option=$(this).val()
+                }
+                $(this).show();
+                index++
+            } else {
+                $(this).hide();
+            }
+            
         });
-        $('#parent_id').val('').trigger('change');
+        $('#parent_id').val(first_option).trigger('change');
         // $('#farmer-station').select2();
     });
     // $('#farmer-group').select2({
