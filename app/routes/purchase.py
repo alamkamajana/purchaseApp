2e468e78e63335aeaef1463523f9324b8492f918
administrator <wayanpuniaraharja@gmail.com>
2024-05-30 14:12:26 +0800

    merge3

--- app/routes/purchase.py
+++ app/routes/purchase.py
@@ -173,9 +173,22 @@ def transaction_order_add():
 def transaction_list():
     try :
         event_id = request.args.get('pe', 0, type=int)
-        purchase_lists = PurchaseOrder.query.filter_by(purchase_event_id=event_id).all()
+        purchase_lists = PurchaseOrder.query.filter_by(purchase_event_id=event_id).order_by(PurchaseOrder.id.desc()).all()
         event_obj = PurchaseEvent.query.filter_by(id=event_id).first()
-        return render_template('purchase/purchase.html', purchase_event=event_obj, purchase_lists=purchase_lists, Farmer=NfcappFarmerOdoo)
+        price_list = []
+        for purchase in purchase_lists:
+            total_price = 0
+            po_lines = PurchaseOrderLine.query.filter_by(purchase_order_id=purchase.id).all()
+            for po_line in po_lines:
+                total_price = total_price + po_line.subtotal
+            price_data_json = {}
+            price_data_json['po_id'] = purchase.id
+            price_data_json['total'] = total_price
+            price_list.append(price_data_json)
+
+        print(price_list)
+
+        return render_template('purchase/purchase.html', purchase_event=event_obj, purchase_lists=purchase_lists, Farmer=NfcappFarmerOdoo, price_list=price_list)
 
     except Exception as e :
         print(e)
@@ -344,7 +357,7 @@ def transaction_detail():
     for commodity_data in commodity_item_product_arr:
         if commodity_data['product_id'] in po_line_product_arr:
             product_can_purchase_arr.append(commodity_data)
-    return render_template('purchase/transaction_detail.html', purchase_order_line=order_line, product_can_purchase = product_can_purchase_arr)
+    return render_template('purchase/transaction_detail.html', purchase_order_line=order_line, product_can_purchase = product_can_purchase_arr, ProductOdoo=ProductOdoo, farmer_id=farmer_id)
 
 
 @bp.route('/transaction/confirm', methods=["POST","GET"])
