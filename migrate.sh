bd198c0afc9f954b3dead2f959e0108d8ebb9637
cerdasjanastu <cerdasjanastu@gmail.com>
2024-06-07 13:36:44 +0800

    update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source venv3/bin/activate
+source venv/bin/activate
 
 # Initialize the database migrations folder
 flask --app run db init
-
 # Generate the migration file
 flask --app run db migrate -m "Update flaskApp"
-
 # Apply the migrations to the database
 flask --app run db upgrade
-
 # Deactivate the virtual environment
 deactivate
+
