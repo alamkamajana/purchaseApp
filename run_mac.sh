0329e88a2d7a4876412f394fd0ee96b72d47e9d1
cerdasjanastu <cerdasjanastu@gmail.com>
2024-06-07 15:25:53 +0800

    update change port to 5001

--- run_mac.sh
+++ run_mac.sh
@@ -11,6 +11,6 @@ source venv/bin/activate
 export FLASK_APP=run.py
 
 # Open the URL in the default browser
-xdg-open "http://$ip:5000" &
+xdg-open "http://$ip:5001" &
 flask run --host="$ip"
 read -rp "Press any key to continue..."   # Pause the script until a key is pressed
