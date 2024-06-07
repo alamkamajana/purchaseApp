7bf09a342f8a2a97f6e0643c2fc657b6cd39b6c6
cerdasjanastu <cerdasjanastu@gmail.com>
2024-06-07 13:37:51 +0800

    add run_windows.bat

---
+++ run_windows.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Running the app...
+
+REM set ip address
+for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /c:"IPv4 Address"') do set ip=%%i
+set ip=%ip:~1%
+
+cd /d %~dp0
+call venv\Scripts\activate
+set FLASK_APP=run.py
+
+start "" http://%ip%:5000
+flask run --host=%ip%
+pause
