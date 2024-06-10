3b07eb2f616c266d78c056884f863d440f8444cb
cerdasjanastu <cerdasjanastu@gmail.com>
2024-06-10 11:55:11 +0800

    update add ssl_context

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=5001)
+    context = ("cert.crt", "cert.key")
+    app.run(debug=False, host="0.0.0.0", port=5001, ssl_context=context)
